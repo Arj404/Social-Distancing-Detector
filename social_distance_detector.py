485d9522a1b0f5069e83156c5ed407d8f954e200
Arjav Jain <arjavjain598@gmail.com>
2020-06-11 00:19:38 +0530

    speed improved

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -6,8 +6,9 @@ import argparse
 import imutils
 import cv2
 import os
+import time
 
-
+start = time.time()
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", type=str, default="",
                 help="path to (optional) input video file")
@@ -17,6 +18,7 @@ ap.add_argument("-d", "--display", type=int, default=1,
                 help="whether or not output frame should be displayed")
 args = vars(ap.parse_args())
 
+
 labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
@@ -25,15 +27,20 @@ net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-print("[INFO] accessing video stream...")
+
 vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
 writer = None
+count = 0
 
 while True:
     (grabbed, frame) = vs.read()
+    count = count + 1
+    if count%10 != 0:
+        continue
+    
     if not grabbed:
         break
-    frame = imutils.resize(frame, width=700)
+    frame = imutils.resize(frame, width=400)
     results = detect_people(frame, net, ln,
                             personIdx=LABELS.index("person"))
 
@@ -42,7 +49,7 @@ while True:
     if len(results) >= 2:
         centroids = np.array([r[2] for r in results])
         height = np.array([r[1][3]-r[1][1] for r in results])
-        min_distance = np.array([(h*23)/100 for h in height])
+        min_distance = np.array([(h)/3 for h in height])
         D = dist.cdist(centroids, centroids, metric="euclidean")
 
         for i in range(0, D.shape[0]):
@@ -60,14 +67,15 @@ while True:
         if i in violate:
             color = (0, 0, 255)
 
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-        cv2.circle(frame, (cX, cY), 5, color, 1)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 1)
+        cv2.circle(frame, (cX, cY), 3, color, 1)
 
     text = "Social Distancing Violations: {}".format(len(violate))
     cv2.putText(frame, text, (10, frame.shape[0] - 25),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
 
     if args["display"] > 0:
+        frame = imutils.resize(frame, width=700)
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
@@ -80,3 +88,6 @@ while True:
 
     if writer is not None:
         writer.write(frame)
+
+
+print(f"time taken = {time.time()-start}")
